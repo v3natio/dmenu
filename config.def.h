3e12e3741e120bee4d1156d0dd601a34d9a2832d
v3natio <72844565+v3natio@users.noreply.github.com>
2025-06-15 00:12:09 -0500

    added center patch

--- config.def.h
+++ config.def.h
@@ -1,8 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static int topbar = 1;                        /* -b  option; if 0, dmenu appears at bottom */
+static int centered = 1;                      /* -c option; centers dmenu on screen */
+static int min_width = 500;                   /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+static const unsigned int alpha = 0xff;       /* Amount of opacity. 0xff is opaque */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
