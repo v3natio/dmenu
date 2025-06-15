a2fe3f40b3443136236975e6d5ae770e6b3c2d6b
v3natio <72844565+v3natio@users.noreply.github.com>
2025-06-15 17:07:01 -0500

    reverted xresources patch

--- config.h
+++ config.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                        /* -b  option; if 0, dmenu appears at bottom */
-static int centered = 1;                      /* -c option; centers dmenu on screen */
-static int min_width = 1000;                   /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
-static const unsigned int alpha = 0xf0;       /* Amount of opacity. 0xff is opaque */
+static int topbar = 1; // -b  option; if 0, dmenu appears at bottom
+static int centered = 1; // -c option; centers dmenu on screen
+static int min_width = 1000; // minimum width when centered
+static const float menu_height_ratio = 4.0f; // ratio used in the original calculation
+static const unsigned int alpha = 0xff; // opacity, 0xff is opaque
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mono:pixelsize=13:antialias=true:autohint=true"
+  "JetbrainsMono Nerd Font:size=13:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+  /* foreground, background */ 
+  [SchemeNorm] = { "#928374", "#282828" },
+  [SchemeSel] = { "#282828", "#928374" },
+  [SchemeSelHighlight] = { "#fabd2f", "#928374" },
+  [SchemeNormHighlight] = { "#fabd2f", "#282828" },
+  //[SchemeOut] = { "#000000", "#00ffff" },
+  //[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
